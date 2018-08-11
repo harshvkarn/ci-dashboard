a8d093db99a2d32c8b8f8df4b3176d8a96bf9f47
harshvkarn <harshvkarn54@gmail.com>
2018-08-11 15:14:01 +0530

    Add functionality to populate data from api/json
    Signed-off-by: harshvkarn <harshvkarn54@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CI-Dashboard';
-  
+  restItems: any;
+  restItemsUrl = 'http://localhost:3001/rest/v1/api/v4/dashboard';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  getRestItems(): void {
+    this.restItemsServiceGetRestItems()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      )
+  }
+
+  // Rest Items Service: Read all REST Items
+  restItemsServiceGetRestItems() {
+    return this.http
+      .get<any[]>(this.restItemsUrl)
+      .pipe(map(data => data));
+  }
+  buttonClass(status) {
+    if(status=="success")
+           return "btn btn-round btn-success";
+    else if(status=="pending")
+        return "btn btn-round btn-basic";
+    else if(status=="failed")
+        return "btn btn-round btn-danger";
+    else if(status=="running")
+        return "btn btn-round btn-primary";
+   }
+   iconClass(status) {
+    if(status=="success")
+           return "fa fa-check-circle";
+    else if(status=="pending")
+        return "fa fa-clock-o";
+    else if(status=="failed")
+        return "fa fa-exclamation-triangle";
+    else if(status=="running")
+        return "fa fa-circle-o-notch fa-spin"
+    console.log('test');
+    
+   }
+   clickit(url) {
+     location.href = url;
+   }
+   GetClick() {
+     alert('hi');
+   }
 }
