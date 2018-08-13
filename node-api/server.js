2337d31b3151bcdb38d81f639e3f5e46f084f2a8
Chandan Kumar <chandan.kr404@gmail.com>
2018-08-13 19:02:34 +0530

    modify server.js
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- node-api/server.js
+++ node-api/server.js
@@ -1,59 +1,96 @@
-var async = require("async");
-var request = require("request");
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var router = express.Router();
+// var async = require("async");
+// var request = require("request");
+// var express = require('express');
+// var app = express();
+// var port = process.env.PORT || 8080;
+// var router = express.Router();
 
-// create request objects
-var requests = [{
-        url: 'https://gitlab.com/api/v4/projects/7642571/pipelines',
-        headers: {
-          'PRIVATE-TOKEN': 'K1Hdz5g9SqwuW7btpkzh'
-        }
-    }  
-    // {
-    //     url: 'https://gitlab.com/api/v4/projects/7642571',
-    //     headers: {
-    //       'PRIVATE-TOKEN': 'K1Hdz5g9SqwuW7btpkzh'
-    //     }
-    // }
-];
-var id = 0;
-setInterval(function() {
-async.map(requests, function(obj, callback) {
-    // iterator function
-    request(obj, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // transform data here or pass it on
-        data = JSON.parse(body);
-        var mdata = [];
-        for (var i = 0; i < 10; i++) {
-            mdata[i] = [{ 'commit' : data[i].sha, 'build' : { 'id' : data[i].id, 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'aws' : { 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'gcp' : { 'status' : data[i].status, 'web_url' : data[i].web_url }}];
-            // console.log("length-->", data[i]);
-        }
-        callback(null, mdata);
-      } else {
-        callback(error || response.statusCode);
-      }
-    });
-  }, function(err, results) {
-    // all requests have been made
-    if (err) {
-        console.log("error ", err);
-    } else {
-        if (id==0){
-        router.get('/', function(req, res) {
-            res.json(results);	
-        });
-        app.use('/api', router);
-        app.listen(port);
-        console.log('server started on port ' + port);
-        // for (var i = 0; i < results[0].length; i++) {
-        //     // console.log(results[0].length)
-        // }
-        id = 1;
-      }
+// // var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+// // create request objects
+// // setInterval(function() {
+// var requests = [{
+//         url: 'https://gitlab.com/api/v4/projects/7642571/pipelines',
+//         headers: {
+//           'PRIVATE-TOKEN': 'K1Hdz5g9SqwuW7btpkzh'
+//         }
+//     }  
+//     // {
+//     //     url: 'https://gitlab.com/api/v4/projects/7642571',
+//     //     headers: {
+//     //       'PRIVATE-TOKEN': 'K1Hdz5g9SqwuW7btpkzh'
+//     //     }
+//     // }
+// ];
+// async.map(requests, function(obj, callback) {
+//     // iterator function
+//     request(obj, function(error, response, body) {
+//       if (!error && response.statusCode == 200) {
+//         // transform data here or pass it on
+//         data = JSON.parse(body);
+//         var mdata = [];
+//         console.log("commit-->>", data[0].sha);
+//         for (var i = 0; i < 10; i++) {
+//             mdata[i] = [{ 'commit' : data[i].sha, 'build' : { 'id' : data[i].id, 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'aws' : { 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'gcp' : { 'status' : data[i].status, 'web_url' : data[i].web_url }}];
+//             // console.log("length-->", data[i]);
+//         }
+//         callback(null, mdata);
+//       } else {
+//         callback(error || response.statusCode);
+//       }
+//     });
+//   }, function(err, results) {
+//     // all requests have been made
+//     if (err) {
+//         console.log("error ", err);
+//     } else {
+//         io.on('/', function(req, res) {
+//             res.json(results);	
+//         });
+//         app.use('/api', router);
+//         app.listen(port);             
+//         console.log('server started on port ' + port);
+//         // for (var i = 0; i < results[0].length; i++) {
+//         //     // console.log(results[0].length)
+//         // }
+//       // }
+//     }
+//   });
+
+//   // https://code.tutsplus.com/tutorials/real-time-sports-application-using-nodejs--cms-30594
+var port = process.env.PORT || 8080;
+var express = require("express"); 
+var app = express();
+var request = require('request');
+
+var options = {  
+    url: 'https://gitlab.com/api/v4/projects/7642571/pipelines',
+    method: 'GET',
+    headers: {
+        'PRIVATE-TOKEN': 'K1Hdz5g9SqwuW7btpkzh'
+        // 'Accept-Charset': 'utf-8',
+        // 'User-Agent': 'my-reddit-client'
     }
+};
+setInterval(function() {
+  app.get("/api", function(req, res)  { 
+    request(options, function(err, output, body) {  
+      var data = JSON.parse(body);
+      var mdata = [];
+      for (var i = 0; i < 10; i++) {
+        mdata[i] = [{ 'commit' : data[i].sha, 'build' : { 'id' : data[i].id, 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'aws' : { 'status' : data[i].status, 'web_url' : data[i].web_url } }, { 'gcp' : { 'status' : data[i].status, 'web_url' : data[i].web_url }}];
+      }
+      // console.log(json); // Logging the output within the request function
+      res.json(mdata); //then returning the response.. The request.json is empty over here
+    }); //closing the request function
   });
-}, 2000);
+  // console.log("test");
+},1000 );
+
+app.listen(port, function() {  
+    console.log("server is listening on port:", port);
+});
+
+module.exports = app;
